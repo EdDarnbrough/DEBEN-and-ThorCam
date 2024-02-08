ed90999d56adfea8f08852b0a161112de562d585
EdDarnbrough <51373340+EdDarnbrough@users.noreply.github.com>
2024-02-08 13:49:39 +0000

    Update OutputFigure
    Change naming convention to tensile and to match data. Comment out sample height again could be modified to be used later.

--- OutputFigure.m
+++ OutputFigure.m
@@ -1,7 +1,14 @@
-%% Dr Ed Darnbrough University of Oxford Materials Department 2022
+%% Dr Ed Darnbrough University of Oxford Materials Department 2024
 % Load images and data then look at scaling
-[video, video_info, DebenData, Deben_info] = LoadDataAndVideo;
-[Zoom2mm, Full_Im, Zoomed_Im] = PixelScalingErrFunc; 
+[video, video_info, TensileData, Tensile_info] = LoadDataAndVideo;
+% Getting scaling
+Starting_frame = read(video,1);
+grip.location_horizontal = 200:1200; %pixel range containing full grip width
+grip.location_vertical = 1:600; %pixel range containing just top grip
+grip.width_px = diff(ECFedgefit(Starting_frame,1,grip.location_horizontal,grip.location_vertical)); 
+grip.width_mm = 16; %measured by user with a sensible instrument 
+Zoom2mm = grip.width_px/grip.width_mm; 
+%%
 run QuickLook.m 
 
 %% Now improve the fitting if the data set is ok
@@ -13,46 +20,46 @@ run FittedEdges.m
 run BarrellEdge.m 
 
 %% Get sample height
-% Assume that the sample is darker than background and that the gap is and
-% is in the centre of your image
-% below the sample in the Zoomed_Im 
-% if not use dummy.flat = sum(Zoomed_Im(end:-1:1,:,1),2);
-
-dummy.half_size = 50; %only sample in the middle 100 pixels of the image
-dummy.middle = round(size(Zoomed_Im,2)./2)-dummy.half_size:round(size(Zoomed_Im,2)./2)+dummy.half_size; 
-dummy.height = ECFedgefit(Zoomed_Im,2,dummy.middle); %gap above sample
-dummy.gap = ECFedgefit(Zoomed_Im,2,50); %gap between grips
-
-video_info.Sample_height_px = range(dummy.gap)-range(dummy.height);
-Deben_info.Sample_height_mm = video_info.Sample_height_px./Zoom2mm;
+% % Assume that the sample is darker than background and that the gap is and
+% % is in the centre of your image
+% % below the sample in the Zoomed_Im 
+% % if not use dummy.flat = sum(Zoomed_Im(end:-1:1,:,1),2);
+% 
+% dummy.half_size = 50; %only sample in the middle 100 pixels of the image
+% dummy.middle = round(size(Zoomed_Im,2)./2)-dummy.half_size:round(size(Zoomed_Im,2)./2)+dummy.half_size; 
+% dummy.height = ECFedgefit(Zoomed_Im,2,dummy.middle); %gap above sample
+% dummy.gap = ECFedgefit(Zoomed_Im,2,50); %gap between grips
+% 
+% video_info.Sample_height_px = range(dummy.gap)-range(dummy.height);
+% Tensile_info.Sample_height_mm = video_info.Sample_height_px./Zoom2mm;
 %% Convert all measurements into mm and then stress and strain (after you have sample height, see bottom)
 
-DebenData.Width_mm = interp1(video_info.FrameTime(Times),Width(Times)./Zoom2mm,DebenData.Sec);
+TensileData.Width_mm = interp1(video_info.FrameTime(Times),Width(Times)./Zoom2mm,TensileData.Time);
 
-DebenData.Gap_mm = interp1(video_info.FrameTime(Times),Gap(Times)./Zoom2mm,DebenData.Sec);
+TensileData.Gap_mm = interp1(video_info.FrameTime(Times),Gap(Times)./Zoom2mm,TensileData.Time);
 
-DebenData.Stress_Pa = DebenData.Force./(pi.*(0.5*DebenData.Width_mm.*10^(-3)).^2);
+TensileData.Stress_Pa = TensileData.Force./((TensileData.Width_mm.*10^(-3)).^2); %assumed square sectioned
 
-DebenData.Strain = DebenData.Gap_mm./Deben_info.Sample_height_mm; 
+TensileData.Strain = TensileData.Gap_mm./min(TensileData.Gap_mm,[],'omitnan'); %1D strain
 
 %display result where negative strain is before contact and positive strain
-%is compressive
-figure, plot(1-DebenData.Strain, DebenData.Stress_Pa, 'o')
+%is tensile
+figure, plot(TensileData.Strain-1, TensileData.Stress_Pa, 'o')
 title('Stress Strain')
 xlabel('Strain (-)')
 ylabel('Stress (Pa)')
 
-figure, plot(DebenData.Sec, DebenData.Stress_Pa, 'o')
+figure, plot(TensileData.Time, TensileData.Stress_Pa, 'o')
 title('Stress Time')
 xlabel('Time (s)')
 ylabel('Stress (Pa)')
 
-figure, plot(DebenData.Sec, DebenData.Strain, 'o')
+figure, plot(TensileData.Time, TensileData.Strain, 'o')
 title('Strain Time')
 xlabel('Time (s)')
 ylabel('Strain (-)')
 
 % Select linear section as elastic modulus start strain 0.04 end 0.14
-% dummy.elastic(1) =find(1-DebenData.Strain>0.04,1); dummy.elastic(2) =find(1-DebenData.Strain>0.14,1);
-% [m,c,dm,dc,r] = linfit((1-DebenData.Strain(dummy.elastic(1):dummy.elastic(2))),DebenData.Stress_Pa(dummy.elastic(1):dummy.elastic(2)),0.1.*ones(1+range(dummy.elastic),1));
+% dummy.elastic(1) =find(1-TensileData.Strain>0.04,1); dummy.elastic(2) =find(1-TensileData.Strain>0.14,1);
+% [m,c,dm,dc,r] = linfit((1-TensileData.Strain(dummy.elastic(1):dummy.elastic(2))),TensileData.Stress_Pa(dummy.elastic(1):dummy.elastic(2)),0.1.*ones(1+range(dummy.elastic),1));
 
