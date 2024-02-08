ff1bf17952e276db20809085817d624ddae2f1c1
EdDarnbrough <51373340+EdDarnbrough@users.noreply.github.com>
2024-02-08 09:36:11 +0000

    Update Load script
    Changes to work with slightly different data type

--- LoadDataAndVideo.m
+++ LoadDataAndVideo.m
@@ -1,6 +1,6 @@
-%% Dr Ed Darnbrough University of Oxford Materials Department 2022
+%% Dr Ed Darnbrough University of Oxford Materials Department 2024
 %% Allows user to select the files to work with loading in the data and file information
-function [video, video_info, DebenData, Deben_info] = LoadDataAndVideo
+function [video, video_info, TensileData, Tensile_info] = LoadDataAndVideo
 
 StartingFolder= cd;
 [file,path] = uigetfile('*.avi', 'Select the video file');
@@ -16,37 +16,33 @@ end
 clear path file 
 cd(StartingFolder) %return
 
-[file,path] = uigetfile('*.*', 'Select the Deben Data file');
+[file,path] = uigetfile('*.*', 'Select the Data file');
 
 if isequal(file,0)
    disp('User selected Cancel');
-elseif strcmp(file(end-2:end),'CSV') == 1
+elseif strcmp(file(end-2:end),'csv') == 1
    disp(['User selected ', fullfile(path,file)]);
    cd(path)
-   % Create options for importing data CSV into a table
-   opts = delimitedTextImportOptions("NumVariables", 7);
-
+   %% Set up the Import Options and import the data
+   opts = delimitedTextImportOptions("NumVariables", 3);
+    
    % Specify range and delimiter
-   opts.DataLines = [21, Inf];
+   opts.DataLines = [4, Inf];
    opts.Delimiter = ",";
     
    % Specify column names and types
-   opts.VariableNames = ["Sec", "Elongation", "Force", "Position", "Code", "Samplerate", "Motorspeed"];
-   opts.VariableTypes = ["double", "double", "double", "double", "double", "double", "double"];
+   opts.VariableNames = ["Time", "Force", "Stroke"];
+   opts.VariableTypes = ["double", "double", "double"];
     
    % Specify file level properties
    opts.ExtraColumnsRule = "ignore";
    opts.EmptyLineRule = "read";
-    
-   % Specify variable properties
-   opts = setvaropts(opts, "Motorspeed", "TrimNonNumeric", true);
-   opts = setvaropts(opts, "Motorspeed", "ThousandsSeparator", ",");
-   
+      
    % Import the data
-   DebenData = readtable(file, opts);
-   Deben_info = dir(file);
+   TensileData = readtable(file, opts);
+   Tensile_info = dir(file);
 
-elseif strcmp(file(end-2:end),'txt') == 1
+elseif strcmp(file(end-2:end),'txt') == 1 % not yet updated for Instron Tensile data - probably won't work
     disp(['User selected ', fullfile(path,file)]);
     cd(path)
     %% Set up the Import Options and import the data
@@ -69,8 +65,8 @@ elseif strcmp(file(end-2:end),'txt') == 1
     opts = setvaropts(opts, ["VarName4", "VarName5"], "EmptyFieldRule", "auto");
     
     % Import the data
-    DebenData = readtable(file, opts);
-    Deben_info = dir(file);
+    TensileData = readtable(file, opts);
+    Tensile_info = dir(file);
 else 
     disp('User picked a file with no data.');
 end
@@ -80,9 +76,9 @@ clear opts path file
 cd(StartingFolder) %return
 
 [~, ~, ~, video_info.h, video_info.m, video_info.s] = datevec(video_info.datenum);
-[~, ~, ~, Deben_info.h, Deben_info.m, Deben_info.s] = datevec(Deben_info.datenum);
-video_info.Time_ofset_s = (Deben_info.h*3600+ Deben_info.m*60 + Deben_info.s)-(video_info.h*3600+ video_info.m*60+ video_info.s); %if positive then DEBEN data longer than video, if negative video longer than data
-video_info.starttime = max(DebenData.Sec) - (video.Duration+video_info.Time_ofset_s);
+[~, ~, ~, Tensile_info.h, Tensile_info.m, Tensile_info.s] = datevec(Tensile_info.datenum);
+video_info.Time_ofset_s = (Tensile_info.h*3600+ Tensile_info.m*60 + Tensile_info.s)-(video_info.h*3600+ video_info.m*60+ video_info.s); %if positive then data longer than video, if negative video longer than data
+video_info.starttime = max(TensileData.Time) - (video.Duration+video_info.Time_ofset_s);
 video_info.FrameTime = ([1:video.NumFrames]./video.FrameRate)+video_info.starttime;
 
 end
